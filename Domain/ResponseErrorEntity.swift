0fda3b6656b887605ead62d4fee6fb874b8746d6
Dmytro <dmitry.k792@hotmail.com>
2020-11-18 08:47:01 +0200

    added common error entitiy ResponseErrorEntity

---
+++ Domain/ResponseErrorEntity.swift
@@ -0,0 +1,30 @@
+//
+//  ResponseErrorEntity.swift
+//  GHSR
+//
+//  Created by Dmytro.k on 11/16/20.
+//
+
+import Foundation
+
+public class ResponseErrorEntity: Error, LocalizedError {
+    
+    public var errors = [String]()
+    
+    public let urlResponse: URLResponse?
+    
+    public let urlRequest: URLRequest?
+    
+    public var statusCode: Int? {
+        return (self.urlResponse as? HTTPURLResponse)?.statusCode
+    }
+    
+    public var errorDescription: String? {
+        return errors.joined()
+    }
+
+    public init(_ urlResponse: URLResponse? = nil, _ urlRequest: URLRequest? = nil) {
+        self.urlResponse = urlResponse
+        self.urlRequest = urlRequest
+    }
+}
